fb75674f71d3533849bd4c6c7713ac63e0f99cff
Михаил Емцев <vrnvoid@gmail.com>
2023-06-15 01:06:44 +0300

    added polydromes definition function

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-// Функция для проверки длинны строки.
+// Функция для проверки длинны строки:
 
 function comparisondLength(stringLength, maxLength) {
   if (stringLength.length <= maxLength) {
@@ -9,3 +9,20 @@ function comparisondLength(stringLength, maxLength) {
 
 // eslint-disable-next-line
 console.log(comparisondLength('проверяемая строка', 18));
+
+// Функция для проверки, является ли строка палиндромом:
+
+const checkPalindrome = function (palindrome) {
+  const textClean = palindrome.toLowerCase().replaceAll(' ', '');
+
+  const lastIndex = textClean.length - 1;
+  for (let i = 0; i <= textClean.length / 2; i++) {
+    if (textClean[i] === textClean[lastIndex - i]) {
+      return true;
+    }
+    return false;
+  }
+};
+
+// eslint-disable-next-line
+console.log(checkPalindrome ('Лёша на полке клопа нашёл '));
